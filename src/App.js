425216bcda03ef19b4a71e40c85516032d7d5dfe
1Fe <dnjscjf0829@naver.com>
2024-07-24 22:34:43 +0900

    [feat]SideMenu 구현

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import './App.css';
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import './App.css';
+import SideMenu from "./components/SideMenu";
+import Home from "./pages/Home";
+import Create from "./pages/Create";
+import Library from "./pages/Library";
+import Social from "./pages/Social";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <Router>
+      <div className="App">
+        <SideMenu />
+        <Routes>
+          <Route exact path="/" component={Home} /> */
+          <Route exact path="/create" component={Create} />
+          <Route exact path="/library" element={Library} />
+          <Route exact path="/social" component={Social} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/styles/Theme.js
+++ src/styles/Theme.js
@@ -1,23 +1,39 @@
 import { css } from "styled-components";
-import "./font.css";
+import "./Font.css";
 
 export const Theme = {
-    fonts: {
-        button: css`
-            font-family: "Noto Sans KR", sans-serif;
-            font-weight: 600;
-            font-size: 36px;
-        `,
-        subTitle: css`
-            font-family: "Noto Sans KR", sans-serif;
-            font-weight: 600;
-            font-size: 20px;
-        `,
-    },
+  fonts: {
+    button: css`
+      font-family: "Noto Sans KR", sans-serif;
+      font-weight: 600;
+      font-size: 20px;
+    `,
+    label: css`
+      font-family: "Noto Sans KR", sans-serif;
+      font-weight: 700;
+      font-size: 15px;
+    `,
+    footerText: css`
+      font-family: "Noto Sans KR", sans-serif;
+      font-weight: 800;
+      font-size: 12px;
+    `,
+    navLink: css`
+      font-family: "FTLAB-HOON";
+      font-weight: 500;
+      font-size: 35px;
+    `,
+  },
 
-    colors: {
-        gray: "#717171",
-        red: "#D72F2F",
-        green: "#2BCD3B",
-    },
+  colors: {
+    black: "#000000",
+    white: "#FFFFFF",
+    gray: "#717171",
+    red: "#D72F2F",
+    green: "#2BCD3B",
+    lightBlue: "#81D8F3",
+    darkBlue: "#357ae8",
+    borderGray: "#C2C2C2",
+    yellow: "#FFD439",
+  },
 };
