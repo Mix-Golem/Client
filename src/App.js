d5ef4f003815c2a0b4ee90380e5e2b30c61b4a5e
youminki <dbalsrl7648@naver.com>
2024-08-03 04:53:56 +0900

    fix : path router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
-
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from '../src/styles/GlobalStyle.js';
 import { Theme } from '../src/styles/Theme.js';
 import './App.css';
+import Create from './pages/Create';
 import FindId from './pages/FindId';
-import SideMenu from "./components/SideMenu";
-import Home from "./pages/Home";
-import Create from "./pages/Create";
-import Library from "./pages/Library";
-import Social from "./pages/Social";
 import FindPassword from './pages/FindPassword';
+import Home from './pages/Home';
+import Library from './pages/Library';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
-
+import Social from './pages/Social';
 
 const App = () => {
   return (
@@ -27,10 +24,10 @@ const App = () => {
           <Route path='/signup' element={<Signup />} />
           <Route path='/findid' element={<FindId />} />
           <Route path='/findpassword' element={<FindPassword />} />
-                      <Route exact path="/" component={<Home />} />
-          <Route exact path="/create" component={<Create />} />
-          <Route exact path="/library" element={<Library />} />
-          <Route exact path="/social" composnent={<Social />} />
+          <Route exact path='/home' component={<Home />} />
+          <Route exact path='/create' component={<Create />} />
+          <Route exact path='/library' element={<Library />} />
+          <Route exact path='/social' composnent={<Social />} />
         </Routes>
       </Router>
     </ThemeProvider>
