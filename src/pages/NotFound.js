db3c1a53447faaa6018d0cef3f969d6f1c11e12f
youminki <dbalsrl7648@gmail.com>
2024-08-14 17:40:46 +0900

    fix : smallButton css

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -3,17 +3,26 @@ import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 import NotFoundImage from '../img/404.svg';
 import { Theme } from '../styles/Theme.js';
+import Button from '../components/LoginJoin/SmallButton';
 
 const NotFound = () => {
   const navigate = useNavigate();
+
+  const handleGoHome = () => {
+    navigate('/');
+  };
 
   return (
     <NotFoundContainer>
       <img src={NotFoundImage} alt='404 Not Found' />
       <NotFoundText>
         <div>Page not found</div>
+        <div className='spacer'></div>
         <div>페이지를 찾을 수 없습니다</div>
       </NotFoundText>
+      <ButtonWrapper>
+        <Button onClick={handleGoHome}>Go Home</Button>
+      </ButtonWrapper>
     </NotFoundContainer>
   );
 };
@@ -32,7 +41,17 @@ const NotFoundContainer = styled.div`
 `;
 
 const NotFoundText = styled.div`
-  margin-top: 40px;
+  margin-top: 20px;
   ${Theme.fonts.title}
   text-shadow: 2px 2px ${Theme.colors.lightGray};
+
+  .spacer {
+    margin: 15px 0;
+  }
+`;
+
+const ButtonWrapper = styled.div`
+  margin-top: 30px;
+  display: flex;
+  justify-content: center;
 `;
